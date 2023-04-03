50223aaf4a1dbf5aa84603ab1f81ac22f7a46fc9
Enator18 <62544405+Enator18@users.noreply.github.com>
2023-04-03 17:41:26 -0400

    set up the project

--- src/main/java/net/enator18/sbrt/SbrtUtils.java
+++ src/main/java/net/enator18/sbrt/SbrtUtils.java
@@ -1,14 +1,17 @@
-package net.fabricmc.example;
+package net.enator18.sbrt;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class SbrtUtils implements ModInitializer {
+
+	public static final String MODID = "sbrt";
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
 	@Override
 	public void onInitialize() {
--- src/main/java/net/enator18/sbrt/mixin/ExampleMixin.java
+++ src/main/java/net/enator18/sbrt/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.enator18.sbrt.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.enator18.sbrt.SbrtUtils;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		SbrtUtils.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
